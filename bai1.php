6c52e20ca2e643cbc9be026db41fb36075c215de
xaocho0987 <63099187+xaocho0987@users.noreply.github.com>
2023-02-24 14:41:18 +0700

    first commit

---
+++ bai1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+        <?php 
+
+
+            // Nhập m,n
+                $m=5;$n=5;
+                echo "Chiều cao: .$m\n";
+                echo "Chiều rộng: .$n\n";
+
+            echo "Tự học";
+            for ($i=0; $i < $m; $i++) { 
+                for ($j=0; $j < $n; $j++) { 
+                    echo " ";
+                }
+                for ($x=0; $x < ; $x++) { 
+                    echo "*";
+                }
+                echo "\n";
+            }
+
+        ?>
+        
+    </div>   
+</body>
+</html>
