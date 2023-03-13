0d0aa632f07fc59d9cbd5fd4dd5fd821e33c5603
xaocho0987 <63099187+xaocho0987@users.noreply.github.com>
2023-03-13 21:12:33 +0700

    sửa

--- baiTH3.php
+++ baiTH3.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Form</title>
 </head>
 
 <body>
@@ -14,45 +14,51 @@
     $fullname = $phone = $email = $income = $city = $county = $branch = "";
     $fullnameErr = $phoneErr = $emailErr = $incomeErr = $cityErr = $countyErr = $branchErr = "";
 
+    // Kiểm tra đầu vào
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Họ Tên
         if (empty($_POST["fullname"])) {
-            $fullnameErr = "* Yêu cầu nhập họ và tên";
+            $fullnameErr = "Họ và tên không để trống";
         } else {
             $fullname = inputInfo($_POST["fullname"]);
         }
 
+        // Số điện thoại
         if (empty($_POST["phone"])) {
-            $phoneErr = "* Yêu cầu nhập số điện thoại";
+            $phoneErr = "Số điện thoại không để trống";
         } else {
             $phone = inputInfo($_POST["phone"]);
         }
 
+        // Email
         if (empty($_POST["email"])) {
-            $emailErr = "* Yêu cầu nhập email";
-        } else {
             $email = inputInfo($_POST["email"]);
         }
 
+        // Thu nhập hàng tháng qua chuyển khoản
         if (empty($_POST["income"])) {
-            $incomeErr = "* Yêu cầu nhập thu nhập hàng tháng";
+            $incomeErr = "Không bỏ qua mục này";
         } else {
             $income = inputInfo($_POST["income"]);
         }
 
+        // Tỉnh thành phố
         if (empty($_POST["city"])) {
-            $cityErr = "* Yêu cầu chọn Tỉnh / Thành phố";
+            $cityErr = "Không bỏ qua mục này";
         } else {
             $city = inputInfo($_POST["city"]);
         }
 
+        // Quận huyện
         if (empty($_POST["county"])) {
-            $countyErr = "* Yêu cầu chọn Quận / Huyện";
+            $countyErr = "Không bỏ qua mục này";
         } else {
             $county = inputInfo($_POST["county"]);
         }
 
+        // Chi nhánh
         if (empty($_POST["branch"])) {
-            $branchErr = "* Yêu cầu chọn chi nhánh";
+            $branchErr = "Không bỏ qua mục này";
         } else {
             $branch = inputInfo($_POST["branch"]);
         }
@@ -67,105 +73,125 @@
     }
 
     ?>
+    <style>
+        .form-group>* {
+            width: 50%;
+        }
 
-    <h2 class="text-danger">ĐĂNG KÝ MỞ THẺ NGAY ĐỂ NHÂN QUÀ TẶNG !</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        .form-group {
+            display: flex;
+            flex-direction: row;
+        }
+
+        .form {
+            gap: 10px;
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
+
+    <h5 class="text-danger"><b>ĐĂNG KÝ MỞ THẺ NGAY ĐỂ NHÂN QUÀ TẶNG!</b></h5>
+    <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <div class="form-group">
-            <label for="" class="form-label">Họ và tên: </label>
-            <input type="text" class="form-control" name="fullname">
-            <span class="text-danger"><?php echo $fullnameErr;?></span>
+            <div class="group-label-span">
+                <label for="fullname" class="form-label">Họ và tên </label>
+                <span class="text-danger">*<?php echo $fullnameErr; ?></span>
+            </div>
+            <input id="fullname" type="text" class="form-control" name="fullname">
         </div>
 
         <div class="form-group">
-            <label for="" class="form-label">Số điện thoại: </label>
-            <input type="number" class="form-control" name="phone">
-            <span class="text-danger"><?php echo $phoneErr;?></span>
+            <div class="group-label-span">
+                <label for="phone" class="form-label">Điện thoại </label>
+                <span class="text-danger">*<?php echo $phoneErr; ?></span>
+            </div>
+            <input id="phone" type="number" class="form-control" name="phone">
         </div>
 
         <div class="form-group">
-            <label for="" class="form-label">Email: </label>
-            <input type="email" class="form-control" name="email">
-            <span class="text-danger"><?php echo $emailErr;?></span>
+            <div class="group-label-span">
+                <label for="email" class="form-label">Email </label>
+            </div>
+            <input id="email" type="email" class="form-control" name="email">
         </div>
 
         <div class="form-group">
-            <label for="" class="form-label">Thu nhập hàng tháng qua <strong>chuyển khoản ngân hàng</strong>: </label>
-            <select name="income" id="" class="form-select">
-                <option value="">--- Vui lòng chọn ---</option>
-                <option value="5 triệu">5 triệu</option>
-                <option value="10 triệu">10 triệu</option>
-                <option value="15 triệu">15 triệu</option>
-                <option value="20 triệu">20 triệu</option>
-                <option value="25 triệu">25 triệu</option>
-                <option value="30 triệu">30 triệu</option>
+            <div class="group-label-span">
+                <label for="income" class="form-label">Thu nhập hàng tháng qua <strong>chuyển khoản ngân hàng </strong></label>
+                <span class="text-danger">*<?php echo $incomeErr; ?></span>
+            </div>
+            <select name="income" id="income" class="form-select">
+                <option value=""> Vui lòng chọn</option>
+                <option value="5 triệu">dưới 5 triệu</option>
+                <option value="10 triệu"> dưới 10 triệu</option>
+                <option value="15 triệu">dưới 15 triệu</option>
+                <option value="20 triệu">dưới 20 triệu</option>
+                <option value="25 triệu">dưới 30 triệu</option>
+                <option value="30 triệu">trên 30 triệu</option>
             </select>
-            <span class="text-danger"><?php echo $incomeErr;?></span>
         </div>
 
-        <h3 style="margin-top:20px;">Chi nhánh tư vấn</h3>
+        <h5 style="margin: 0.5rem; margin-left: 0;">Chi nhánh tư vấn</h5>
         <div class="form-group">
-            <label for="" class="form-label">Tỉnh / Thành phố: </label>
-            <select name="city" id="" class="form-select">
-                <option value="">--- Chọn tỉnh, thành phố ---</option>
-                <option value="Nghệ An">Nghệ An</option>
-                <option value="Hải Phòng">Hải Phòng</option>
+            <div class="group-label-span">
+                <label for="city" class="form-label">Tỉnh/Thành phố </label>
+                <span class="text-danger">*<?php echo $cityErr; ?></span>
+            </div>
+            <select name="city" id="city" class="form-select">
+                <option value=""> Chọn tỉnh, thành phố</option>
+                <option value="An Giang">An Giang</option>
+                <option value="Bạc Liêu">Bạc Liêu</option>
+                <option value="Cà Mau">Cà Mau</option>
+                <option value="Đà Nẵng">Đà Nẵng</option>
                 <option value="Hà Nội">Hà Nội</option>
                 <option value="Hà Tĩnh">Hà Tĩnh</option>
-                <option value="Sài Gòn">Sài Gòn</option>
-                <option value="Phú Thọ">Phú Thọ</option>
+                <option value="Hải Phòng">Hải Phòng</option>
+                <option value="Nghệ An">Nghệ An</option>
+                <option value="Thành phố Hồ Chí Minh">Thành phố Hồ Chí Minh</option>
+                <option value="Phú Yên">Phú Yên</option>
+                <option value="Vĩnh Long">Vĩnh Long</option>
             </select>
-            <span class="text-danger"><?php echo $cityErr;?></span>
         </div>
 
         <div class="form-group">
-            <label for="" class="form-label">Quận / Huyện: </label>
-            <select name="county" id="" class="form-select">
-                <option value="">--- Chọn quận, huyện ---</option>
-                <option value="Đô Lương">Đô Lương</option>
+            <div class="group-label-span">
+                <label for="county" class="form-label">Quận/ huyện </label>
+                <span class="text-danger">*<?php echo $countyErr; ?></span>
+            </div>
+            <select name="county" id="county" class="form-select">
+                <option value=""> Chọn quận/ huyện</option>
+                <option value="Nghi Lộc">Nghi Lộc</option>
                 <option value="Nam Đàn">Nam Đàn</option>
-                <option value="Thái Hòa">Thái Hòa</option>
-                <option value="Quỳ Hợp">Quỳ Hợp</option>
                 <option value="Thanh Chương">Thanh Chương</option>
-                <option value="Can Lộc">Can Lộc</option>
+                <option value="Con Cuông">Con Cuông</option>
+                <option value="Tương Dương">Tương Dương</option>
+                <option value="Yên Bái">Yên Bái</option>
             </select>
-            <span class="text-danger"><?php echo $countyErr;?></span>
         </div>
 
         <div class="form-group">
-            <label for="" class="form-label">Chi nhánh: </label>
-            <select name="branch" id="" class="form-select">
-                <option value="">--- Chọn chi nhánh ---</option>
-                <option value="Chi nhánh Nghệ An">Chi nhánh Nghệ An</option>
-                <option value="Chi nhánh Hà Tĩnh">Chi nhánh Hà Tĩnh</option>
-                <option value="Chi nhánh Hà Nội">Chi nhánh Hà Nội</option>
-                <option value="Chi nhánh Sài Gòn">Chi nhánh Sài Gòn</option>
-                <option value="Chi nhánh Phú Thọ">Chi nhánh Phú Thọ</option>
-                <option value="Chi nhánh Thanh Hóa">Chi nhánh Thanh Hóa</option>
+            <div class="group-label-span">
+                <label for="branch" class="form-label">Chi nhánh </label>
+                <span class="text-danger">*<?php echo $branchErr; ?></span>
+            </div>
+            <select name="branch" id="branch" class="form-select">
+                <option value=""> Chọn chi nhánh</option>
+                <option value="An Giang">An Giang</option>
+                <option value="Bạc Liêu">Bạc Liêu</option>
+                <option value="Cà Mau">Cà Mau</option>
+                <option value="Đà Nẵng">Đà Nẵng</option>
+                <option value="Hà Nội">Hà Nội</option>
+                <option value="Hà Tĩnh">Hà Tĩnh</option>
+                <option value="Hải Phòng">Hải Phòng</option>
+                <option value="Nghệ An">Nghệ An</option>
+                <option value="Thành phố Hồ Chí Minh">Thành phố Hồ Chí Minh</option>
+                <option value="Phú Yên">Phú Yên</option>
+                <option value="Vĩnh Long">Vĩnh Long</option>
             </select>
-            <span class="text-danger"><?php echo $branchErr;?></span>
         </div>
-
-
-        <input style="margin-top:20px;" class="btn btn-success" type="submit" value="GỬI THÔNG TIN">
+        <input style="width: fit-content; margin-inline: auto; margin-top:25px; --bs-btn-bg: #de3747; --bs-btn-hover-color: #000;--bs-btn-hover-bg: var(--bs-cyan);" class="btn btn-success" type="submit" value="GỬI THÔNG TIN">
     </form>
 
-
-
-    <?php
-    if(!empty($fullname) && !empty($phone) && 
-        !empty($email) && !empty($income) && 
-        !empty($city) && !empty($county) && 
-        !empty($branch)) {
-        echo "</br><h3>Thông tin bạn đã nhập</h3>";
-        echo "<strong>Họ và tên:</strong> " . $fullname."</br>";
-        echo "<strong>Điện thoại:</strong> " . $phone."</br>";
-        echo "<strong>Email:</strong> " . $email."</br>";
-        echo "<strong>Thu nhập hàng tháng qua chuyển khoản ngân hàng:</strong> " . $income."</br>";
-        echo "<strong>Tỉnh / Thành phố:</strong> " . $city."</br>";
-        echo "<strong>Quận / Huyện:</strong> " . $county."</br>";
-        echo "<strong>Chi nhánh:</strong> " . $branch;
-    }
-    ?>
 </body>
 
 </html>
